4fa4d71e20ee16d82207551c071d03d2744557c4
jeremiahscanlon <jeremiah.j.scanlon@gmail.com>
2016-02-14 03:33:24 -0500

    javascript changes

--- assets/js/javascript.js
+++ assets/js/javascript.js
@@ -16,41 +16,64 @@ document.querySelector('#word').innerHTML = word;
 var alphaId = '<p> ' + alphabet.join('') + ' </p>';
 document.querySelector('#alphabet').innerHTML = alphaId;
 
-var alertCounter = 10;
-var alert = '<p>You have ' + alertCounter + ' wrong guesses.</p>';
-document.querySelector('#alert').innerHTML = alert;
+var wrongCounter = 10;
+var wrong = '<p>You have ' + wrongCounter + ' wrong guesses.</p>';
+document.querySelector('#alertWrong').innerHTML = wrong;
+
+var correctCounter = playWord.length;
+var alertRight = '<p>You have ' + correctCounter + ' letters left to guess to finish the word.</p>';
+document.querySelector('#alertRight').innerHTML = alertRight;
 
 document.onkeypress = function (event) {
 
-	var userGuess = String.fromCharCode(event.keyCode).toLowerCase();
-	
-	var guess = '<p>You guessed: ' + userGuess + '</p>';
-	document.querySelector('#guessed').innerHTML = guess;
-	
+	//if () {
+		var userGuess = String.fromCharCode(event.keyCode).toLowerCase();
+		
+		var guess = '<p>You guessed: ' + userGuess + '</p>';
+		document.querySelector('#guessed').innerHTML = guess;
 
-	var j = alphabet.indexOf(userGuess);
-	if (playWord.indexOf(userGuess) >= 0) {
-			alphabet[j]= '<span class="correct">_</span>'
-			var nice = '<p>Nice!</p>';
-			document.querySelector('#nice').innerHTML = nice;
-		} else {
-			alphabet[j]= '<span class="wrong">X</span>'
-			alertCounter = alertCounter - 1;
-			var alert = '<p>You have ' + alertCounter + ' wrong guesses left. Be Careful!</p>';
-			document.querySelector('#alert').innerHTML = alert;
+		var j = alphabet.indexOf(userGuess);
+		if (playWord.indexOf(userGuess) >= 0) {
+				alphabet[j]= '<span class="correct">_</span>'
+				document.getElementById('nice').style.display = "block";
+				var nice = '<p>Nice!</p>';
+				document.querySelector('#nice').innerHTML = nice;
+				correctCounter = correctCounter - 1;
+				if (correctCounter > 1){
+					var alertRight = '<p>You have ' + correctCounter + ' letters left to guess to finish the word.</p>';
+				} else {
+					var alertRight = '<p>You have ' + correctCounter + ' letter left to guess to finish the word. Concentrate!</p>';
+				}
+				document.querySelector('#alertRight').innerHTML = alertRight;
+				document.getElementById('nice').style.display = "none";
+			} else {
+				alphabet[j]= '<span class="wrong">X</span>'
+				wrongCounter = wrongCounter - 1;
+				if (wrongCounter > 1){
+					var alertWrong = '<p>You have ' + wrongCounter + ' wrong guesses left. Be Careful!</p>';
+				} else {
+					var alertWrong = '<p>You have ' + wrongCounter + ' wrong guess left. Choose Wisely!</p>';
+				}
+				document.querySelector('#alertWrong').innerHTML = alertWrong;
+				document.getElementById('nice').style.display = "none";
+			}
+
+		var alphaId = '<p> ' + alphabet.join('') + ' </p>';
+		document.querySelector('#alphabet').innerHTML = alphaId;
+
+		for (var i = 0; i < playWord.length; i++) {
+			if (playWord[i] == userGuess) {
+				spaces[i] = userGuess;
+			}
 		}
 
+		var word = '<p> ' + spaces.join('') + ' </p>';
+		document.querySelector('#word').innerHTML = word;
+	} //else {
+		//document.querySelector('#word').innerHTML = word;
+	//}
+//}
 
-	var alphaId = '<p> ' + alphabet.join('') + ' </p>';
-	document.querySelector('#alphabet').innerHTML = alphaId;
-
-	for (var i = 0; i < playWord.length; i++) {
-		if (playWord[i] == userGuess) {
-			spaces[i] = userGuess;
-		}
-	}
-
-	var word = '<p> ' + spaces.join('') + ' </p>';
-	document.querySelector('#word').innerHTML = word;
+function startAgain() {
 
 }
